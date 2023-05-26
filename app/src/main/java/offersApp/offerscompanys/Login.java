3b4d1286080a7f56846a9847155de24cd01d588f
Hanialsenwi <Hanialsenwi@github.com>
2023-05-26 23:11:10 +0300

    Adding Signup and Login Activities

--- app/src/main/java/offersApp/offerscompanys/Login.java
+++ app/src/main/java/offersApp/offerscompanys/Login.java
@@ -1,48 +1,64 @@
 package offersApp.offerscompanys;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-import android.widget.Toast;
+import offersApp.offerscompanys.databinding.ActivityLoginBinding;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import offersApp.offerscompanys.databinding.ActivityLogin2Binding;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 public class Login extends AppCompatActivity {
 
-    EditText username;
-    EditText password;
-    Button loginButton;
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        username = findViewById(R.id.username);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.username);
         password = findViewById(R.id.password);
-        loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginBtn = findViewById(R.id.loginButton);
+        gotoRegister = findViewById(R.id.RegisterButton);
+
+
+
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
             }
         });
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkField(email);
+                checkField(password);
+
+            }
+        });
+
+
+
+    }
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
     }
 }
